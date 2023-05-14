b055d0210d0d1ccf5850b369a40d71b863429d64
HEONJIN HA <gkgjswls842@gmail.com>
2023-05-14 17:28:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 15.46 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,17 +1,29 @@
-function solution(arr)
-{
-    var answer = [];
-    //head를 배열의 첫번째요소로 만들고 다음거랑비교
-    let head = arr[0];
-    answer.push(head);
-    for(let i=1;i<arr.length;i++){
-        if(head !== arr[i]){
-            answer.push(arr[i])
-            head = arr[i]
-        }
+// function solution(arr)
+// {
+//     var answer = [];
+//     //head를 배열의 첫번째요소로 만들고 다음거랑비교
+//     let head = arr[0];
+//     answer.push(head);
+//     for(let i=1;i<arr.length;i++){
+//         if(head !== arr[i]){
+//             answer.push(arr[i])
+//             head = arr[i]
+//         }
         
+//     }
+    
+    
+//     return answer;
+// }
+
+function solution(arr) {
+    var answer = [arr[0]];
+
+    for (let i=1; i<arr.length; i++) {
+        if (answer[answer.length - 1] !== arr[i]) {
+            answer.push(arr[i]);
+        }
     }
-    
-    
+
     return answer;
 }
